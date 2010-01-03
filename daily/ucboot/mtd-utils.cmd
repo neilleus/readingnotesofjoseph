d37b238c5c9272ee51c2363261d9294d13d07a55
fdlixiaojun <fdlixiaojun@d50e2bb6-ddb3-11de-a25d-e77b6a08f8c3>
2010-01-03 14:00:02 +0000

    fix bugs

--- daily/ucboot/mtd-utils.cmd
+++ daily/ucboot/mtd-utils.cmd
@@ -1,5 +1,5 @@
 
-bin2nand
+bin2nand		给输入的二进制文件添加OOB信息的工具
 docfdisk
 doc_loadbios
 flashcp			将数据拷贝到NOR闪存
@@ -12,39 +12,39 @@ flash_otp_dump
 flash_otp_info
 ftl_check
 ftl_format
-jffs2dump
-mkbootenv
+jffs2dump		dump JFFS2映像的内容
+mkbootenv		处理bootenv文本文件并将其转化为二进制文件
 mkfs.jffs2		创建JFFS2文件系统映像
-mkfs.ubifs
-mkpfi
+mkfs.ubifs		创建UBIFS文件系统映像
+mkpfi			
 mtd_debug
-mtdinfo
-nand2bin
-nanddump
+mtdinfo			打印mtd信息的工具
+nand2bin		分离数据和OOB信息
+nanddump		dump出nand mtd分区的内容
 nandtest
 nandwrite		向NAND闪存设备写入（某个映像）
 nftldump
-nftl_format
-pddcustomize
-pfi2bin
-pfiflash
-recv_image
-rfddump
-rfdformat
-serve_image
-sumtool
-ubiattach
+nftl_format		
+pddcustomize		自定义bootenv和pdd值
+pfi2bin			将PFI文件转化为二进制映像的工具
+pfiflash		更新带PFI工具的控制器的工具
+recv_image		
+rfddump			dump常驻闪存盘内容的工具
+rfdformat		将NOR闪存格式化为常驻闪存盘
+serve_image		
+sumtool			将jffs2映像转化为带EBS的映像
+ubiattach		将一个mtd设备关联到UBI的工具
 ubicrc32
 ubicrc32.pl
-ubidetach
-ubiformat
-ubigen
-ubimirror
-ubimkvol
-ubinfo
-ubinize
+ubidetach		移除UBI设备（从UBI上写在MTD设备）的工具
+ubiformat		格式化MTD设备和闪存UBI映像的工具
+ubigen			给输入的二进制文件添加UBI信息的工具
+ubimirror		镜像ubi卷的工具
+ubimkvol		创建UBI卷的工具
+ubinfo			打印UBI信息的工具
+ubinize			生成UBI映像的工具
 ubirename
-ubirmvol
-ubirsvol
-ubiupdatevol
-unubi
+ubirmvol		移除UBI卷的工具
+ubirsvol		resize UBI卷的工具
+ubiupdatevol		向UBI卷写入数据的工具
+unubi			从一个UBI数据文件中提取和分析卷信息
