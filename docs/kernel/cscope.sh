9b819b8f0eb4a53387c020ff02a45eeb9289184b
fdlixiaojun <fdlixiaojun@d50e2bb6-ddb3-11de-a25d-e77b6a08f8c3>
2010-01-12 15:42:58 +0000

    update

---
+++ docs/kernel/cscope.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LNX=/home/joseph/linux
+
+cd / 	
+
+find $LNX 						\
+-path "$LNX/arch/*" ! -path "$LNX/arch/x86*" -prune -o	\
+-path "$LNX/tools*" -prune -o  				\
+-path "$LNX/fs*" ! -path "$LNX/fs*/jffs2*" -prune -o 	\
+-path "$LNX/Documentation*" -prune -o 			\
+-path "$LNX/drivers*" -prune -o 			\
+-path "$LNX/firmware*" -prune -o 			\
+-path "$LNX/sound*" -prune -o 				\
+-path "$LNX/include/sound*" -prune -o 			\
+-name *."[chxsS]" -print >$LNX/cscope/cscope.files
+
+cd $LNX/cscope
+
+cscope -b -q -k
